f51a47b77060edd8215be06e469fa6ea70973cdf
GajananBidwai <gajananbidwai525@gmail.com>
2024-01-03 15:34:44 +0530

    Initial Commit

---
+++ Assignment/UsersDetailCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UsersDetailCollectionViewCell.swift
+//  Assignment
+//
+//  Created by Mac on 03/01/24.
+//
+
+import UIKit
+
+class UsersDetailCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var userPageLabel: UILabel!
+    
+    @IBOutlet weak var firstNameLabel: UILabel!
+    
+    @IBOutlet weak var lastNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
